f39b1601c5d49b03c05353f398a9d7819c89ba5a
Nikhil <NikhilC2209@gmail.com>
2024-05-15 03:31:28 +0530

    Reverted to Shiki Syntax Highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,35 +14,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-
-  markdown: {
-    shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://shiki.style/themes
-      theme: 'dracula',
-      // Alternatively, provide multiple themes
-      // https://shiki.style/guide/dual-themes
-      themes: {
-        light: 'github-light',
-        dark: 'github-dark',
-      },
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://shiki.style/languages
-      langs: [],
-      // Enable word wrap to prevent horizontal scrolling
-      wrap: true,
-      // Add custom transformers: https://shiki.style/guide/transformers
-      // Find common transformers: https://shiki.style/packages/transformers
-      transformers: [],
-    },
-  },
-
-  // markdown: {
-  //   // Can be 'shiki' (default), 'prism' or false to disable highlighting
-  //   syntaxHighlight: 'prism',
-  // },
-
   vite: {
     plugins: [basicSsl()],
     server: {
